acd2b70797c07eadfb8ce14b6f0c66929be74c9f
Mehedi Hasan Chonchol <chonchol.am@gmail.com>
2018-10-17 15:32:42 +0600

    Progress bar added

--- app/src/main/java/me/chonchol/andropos/layout/AddCategoryActivity.java
+++ app/src/main/java/me/chonchol/andropos/layout/AddCategoryActivity.java
@@ -1,9 +1,11 @@
 package me.chonchol.andropos.layout;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import dmax.dialog.SpotsDialog;
 import es.dmoral.toasty.Toasty;
 import me.chonchol.andropos.R;
 import me.chonchol.andropos.enums.RequestCode;
@@ -64,17 +67,28 @@ public class AddCategoryActivity extends AppCompatActivity {
     }
 
     private void saveCategory(Category category) {
+
+        final ProgressDialog progressDoalog;
+        progressDoalog = new ProgressDialog(getApplicationContext());
+        progressDoalog.setMax(100);
+        progressDoalog.setMessage("Please wait...");
+        progressDoalog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        // show it
+        progressDoalog.show();
+
         ApiService apiService = ApiClient.getClient().create(ApiService.class);
         apiService.saveCategory(category).enqueue(new Callback<Category>() {
             @Override
             public void onResponse(Call<Category> call, Response<Category> response) {
                 if (response.isSuccessful()){
+                    progressDoalog.dismiss();
                     Toasty.success(getApplicationContext(), "Category added successfully!", Toast.LENGTH_SHORT, true).show();
                 }
             }
 
             @Override
             public void onFailure(Call<Category> call, Throwable t) {
+                progressDoalog.dismiss();
                 Toasty.error(getApplicationContext(), "Category addition failed!!!", Toast.LENGTH_SHORT, true).show();
             }
         });
